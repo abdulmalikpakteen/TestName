e1e066e7b739e8f74531e2d18795dfa4cc545932
abdulmalikpakteen <abdulmalikpakteen@gmail.com>
2018-12-29 11:40:19 -0500

    Abdul

---
+++ src/test/java/com/maven/BaseClass.java
@@ -0,0 +1,26 @@
+package com.maven;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BaseClass {
+	
+	public static WebDriver driver;
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "src/Drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		
+	}
+	public void tearDown() {
+		driver.quit();
+	}
+	
+
+}
---
+++ src/test/java/com/maven/MavenTest.java
@@ -0,0 +1,13 @@
+package com.maven;
+
+import org.testng.annotations.Test;
+
+public class MavenTest {
+	
+	@Test
+	public void hello() {
+		System.out.println("this is my first moaven test");
+	}
+	
+
+}
